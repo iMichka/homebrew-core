338361c9c3e64d55d8eaf3d85c5cbc952ef2f517
Michka Popoff <michkapopoff@gmail.com>
2017-07-10 22:43:33 +0200

    masscan: Build a bottle for Linuxbrew

--- Formula/masscan.rb
+++ Formula/masscan.rb
@@ -1,3 +1,4 @@
+# masscan: Build a bottle for Linuxbrew
 class Masscan < Formula
   desc "TCP port scanner, scans entire Internet in under 5 minutes"
   homepage "https://github.com/robertdavidgraham/masscan/"
