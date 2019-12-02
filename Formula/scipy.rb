49feb8de29b5d6ace2f58d235fcfbb7f39021715
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:01 +0000

    scipy: revision bump for python@3.8

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -18,7 +18,7 @@ class Scipy < Formula
   depends_on "numpy"
   depends_on "openblas"
   depends_on "pybind11"
-  depends_on "python"
+  depends_on "python@3.8"
 
   cxxstdlib_check :skip
 
@@ -39,11 +39,11 @@ class Scipy < Formula
 
     Pathname("site.cfg").write config
 
-    version = Language::Python.major_minor_version "python3"
+    version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
     ENV["PYTHONPATH"] = Formula["numpy"].opt_lib/"python#{version}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
-    system "python3", "setup.py", "build", "--fcompiler=gnu95"
-    system "python3", *Language::Python.setup_install_args(prefix)
+    system Formula["python@3.8"].opt_bin/"python3", "setup.py", "build", "--fcompiler=gnu95"
+    system Formula["python@3.8"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
   end
 
   # cleanup leftover .pyc files from previous installs which can cause problems
@@ -53,6 +53,6 @@ class Scipy < Formula
   end
 
   test do
-    system "python3", "-c", "import scipy"
+    system Formula["python@3.8"].opt_bin/"python3", "-c", "import scipy"
   end
 end
