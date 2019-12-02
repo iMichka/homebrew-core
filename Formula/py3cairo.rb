0748fa4af26fe1b82a62040aa781cc220277a4ca
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:00 +0000

    py3cairo: revision bump for python@3.8

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -3,6 +3,7 @@ class Py3cairo < Formula
   homepage "https://cairographics.org/pycairo/"
   url "https://github.com/pygobject/pycairo/releases/download/v1.19.1/pycairo-1.19.1.tar.gz"
   sha256 "2c143183280feb67f5beb4e543fd49990c28e7df427301ede04fc550d3562e84"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,7 +14,7 @@ class Py3cairo < Formula
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     system "python3", *Language::Python.setup_install_args(prefix)
